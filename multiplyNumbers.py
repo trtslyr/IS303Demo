5dbdfadfc5b5ff9c053c612c42c04b988381a8f0
ratherjr <167947394+ratherjr@users.noreply.github.com>
2025-02-20 10:28:05 -0700

    Created function to multiply two numbers
    This is a function that takes two numbers and multiplies them together

---
+++ multiplyNumbers.py
@@ -0,0 +1,8 @@
+# Joseph Rather
+# 2/20/2025
+# Take input and multiply it
+
+# takes two numbers and returns the product
+def multiplyTwoNumbers(iNum1,iNum2):
+    result = iNum1 * iNum2
+    return result
